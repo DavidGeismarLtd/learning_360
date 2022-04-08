36d4c8d7d10cdf6f58a825121a9a7493886a359c
david geismar <davidgeismar@hotmail.fr>
2022-04-08 15:19:48 +0200

    Adding custom Fields API support

--- lib/learning_360/client/user_custom_fields.rb
+++ lib/learning_360/client/user_custom_fields.rb
@@ -5,6 +5,54 @@ module Learning360
     module UserCustomFields
       BASE = '/customfields'
 
+      def create_custom_field(options = {})
+        response = self.class.post("#{BASE}", {
+                                     body: options.to_query,
+                                     headers: {
+                                       'Content-Type' => 'application/x-www-form-urlencoded'
+                                     }
+                                   })
+        JSON.parse(response.body)
+      end
+
+      def update_custom_field(custom_field_id, options={})
+        response = self.class.put("#{BASE}/#{custom_field_id}", {
+                                    body: options.to_query,
+                                    headers: {
+                                      'Content-Type' => 'application/x-www-form-urlencoded'
+                                    }
+                                  })
+        JSON.parse(response.body)
+      end
+
+      def add_custom_field_value(collection, target_id, options={})
+        response = self.class.put("#{BASE}/#{collection}/#{target_id}", {
+                                    body: options.to_query,
+                                    headers: {
+                                      'Content-Type' => 'application/x-www-form-urlencoded'
+                                    }
+                                  })
+        JSON.parse(response.body)
+      end
+
+      def delete_custom_field(custom_field_id)
+        response = self.class.delete("#{BASE}/#{custom_field_id}", {
+                                       headers: {
+                                         'Content-Type' => 'application/json'
+                                       }
+                                     })
+        JSON.parse(response.body)
+      end
+
+      def remove_custom_field_value(custom_field_id, collection_name, target_id)
+        response = self.class.delete("#{BASE}/#{custom_field_id}/#{collection_name}/#{target_id}", {
+                                       headers: {
+                                         'Content-Type' => 'application/json'
+                                       }
+                                     })
+        JSON.parse(response.body)
+      end
+
       def list_custom_fields
         request(UserCustomField) do
           response = self.class.get(BASE, {
@@ -13,7 +61,7 @@ module Learning360
                                       }
                                     }).body
         end
-      end  
+      end
     end
   end
 end
